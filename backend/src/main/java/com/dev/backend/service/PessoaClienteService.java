62f365f8f01c54dfca1a5a5cbb40265014d9125a
lucas santos <lucas.santos517@fatec.sp.gov.br>
2023-12-20 22:40:42 -0300

    :sparkles: create email confirmation

--- backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
+++ backend/src/main/java/com/dev/backend/service/PessoaClienteService.java
@@ -17,12 +17,15 @@ public class PessoaClienteService {
     private PessoaRepository pessoaRepository;
 
     @Autowired
+    private EmailService emailService;
+    @Autowired
     private PermissaoPessoaService permissaoPessoaService;
     public Pessoa registrar(PessoaClienteRequestDTO pessoaClienteRequestDTO){
         Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
         pessoa.setDataCriacao(new Date());
         Pessoa objetoNovo = pessoaRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente(objetoNovo);
+        emailService.enviarEmailTexto(objetoNovo.getEmail(),"Confirmação de cadastro", "O registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso pro email.");
         return pessoaRepository.saveAndFlush(pessoa);
     }
 
